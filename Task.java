8227f4704acf4c1a0db24facf5b80ea1877ce436
dattatray11Mane <141643062+dattatray11Mane@users.noreply.github.com>
2023-12-15 17:37:50 +0100

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,18 @@
+package com.yourcompany.taskmanager;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    // getters and setters
+}
