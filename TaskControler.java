48ee2bd87e3f75fe0d24b529c28213cc29fb1fb5
dattatray11Mane <141643062+dattatray11Mane@users.noreply.github.com>
2023-12-15 17:39:33 +0100

    Create TaskControler.java

---
+++ TaskControler.java
@@ -0,0 +1,27 @@
+package com.yourcompany.taskmanager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    // other CRUD operations
+}
+
